9779596a7b99dde7a648ee20f4819e4aed1e964f
bariskisir <bariskisir@gmail.com>
2017-10-08 01:01:08 +0300

    zero

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hash Generator
+
+
+Download at [here](https://github.com/bariskisir/hash-generator/releases/download/v1.0.0/hash-generator.exe)
+
+![hash-generator](http://www.bariskisir.com/static/img/posts/hash-generator.jpg)
---
+++ src/hash-generator/Crypto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+namespace hash_generator
+{
+    class Crypto
+    {
+        public static string HashMD5(string text)
+        {
+            if (text == null)
+                return null;
+            var encode = new UTF8Encoding();
+            var md5Hasher = new MD5CryptoServiceProvider();
+            var data = md5Hasher.ComputeHash(encode.GetBytes(text));
+            return byteToHex(data);
+        }
+        public static string HashSHA1(string text)
+        {
+            if (text == null)
+                return null;
+            var encode = new UTF8Encoding();
+            var sha1Hasher = new SHA1CryptoServiceProvider();
+            var data = sha1Hasher.ComputeHash(encode.GetBytes(text));
+            return byteToHex(data);
+        }
+        public static string HashSHA256(string text)
+        {
+            if (text == null)
+                return null;
+            var encode = new UTF8Encoding();
+            var sha256Hasher = new SHA256CryptoServiceProvider();
+            var data = sha256Hasher.ComputeHash(encode.GetBytes(text));
+            return byteToHex(data);
+        }
+        public static string HashSHA384(string text)
+        {
+            if (text == null)
+                return null;
+            var encode = new UTF8Encoding();
+            var sha384Hasher = new SHA384CryptoServiceProvider();
+            var data = sha384Hasher.ComputeHash(encode.GetBytes(text));
+            return byteToHex(data);
+        }
+        public static string HashSHA512(string text)
+        {
+            if (text == null)
+                return null;
+            var encode = new UTF8Encoding();
+            var sha512Hasher = new SHA512CryptoServiceProvider();
+            var data = sha512Hasher.ComputeHash(encode.GetBytes(text));
+            return byteToHex(data);
+        }
+        internal static string byteToHex(byte[] input)
+        {
+            if (input == null)
+                return null;
+            var o = new StringBuilder("");
+            for (var i = 0; i < input.Length; i++)
+                o.Append(input[i].ToString("X2"));
+            return o.ToString();
+        }
+    }
+}
